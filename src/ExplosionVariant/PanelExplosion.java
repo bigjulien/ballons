358a5439db4420e433f6dc6ee3588128847136b4
bigjulien <julienleboss04@hotmail.fr>
2014-01-22 16:31:06 +0100

    uuuuuuuuuuuuuuuuuuuuuuu

--- src/ExplosionVariant/PanelExplosion.java
+++ src/ExplosionVariant/PanelExplosion.java
@@ -1,17 +1,31 @@
 package ExplosionVariant;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.Timer;
 import vehicules.Vehicule;
+import affichage.MyKeyListener;
+import affichage.PanelActionManager;
 import affichage.PanelGenerique;
 import algorithmes.Aleatoire;
 
 public class PanelExplosion extends PanelGenerique{
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
+    public PanelExplosion(){
+        setLayout(new GridLayout(1,10));
+        instanciateArray();       
+        generalStart();     
+        pam=new PanelActionManager(this);        
+        KeyListener listener = new MyKeyListener(pam);
+        addKeyListener(listener);
+        setFocusable(true);
+        timer = new Timer(30,this);
+        timer.start();
+    }
 	
     @Override
 	public void actionPerformed(ActionEvent e) {
